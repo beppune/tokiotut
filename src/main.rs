b3b955729a02c9364b12cbdec064aace3fdf0df5
beppune <manzo.giuseppe@gmail.com>
2026-05-07 10:40:10 +0200

    bho

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,31 @@
+use std::{task::Poll, time::{Duration, Instant}};
 
-use bytes::Bytes;
-use mini_redis::client;
-use tokio::sync::{mpsc, oneshot};
 
-type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+struct Delay {
+    when: Instant,
+}
 
-#[derive(Debug)]
-enum Command {
-   Get {
-       key: String,
-       resp: Responder<Option<Bytes>>,
-   },
-   Set {
-       key: String,
-       val: Bytes,
-       resp: Responder<()>,
-   }
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+
+        if Instant::now() >= self.when {
+            println!("hello, world!");
+            Poll::Ready("done")
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+    
 }
 
 #[tokio::main]
 async fn main() {
-    let (tx, mut rx) = mpsc::channel(32);
-    let tx2 = tx.clone();
+    let when = Instant::now() + Duration::from_millis(10);
+    let future = Delay { when };
 
-    let t1 = tokio::spawn(async move {
-        let (resp_tx, resp_rx) = oneshot::channel();
-        let cmd = Command::Get {
-            key:"foo".to_string(),
-            resp: resp_tx,
-        };
-        tx.send(cmd).await.unwrap();
-
-        let resp = resp_rx.await;
-        print!("got {:?}\n", resp);
-    });
-
-    let t2 = tokio::spawn(async move {
-        let (resp_tx, resp_rx) = oneshot::channel();
-        let cmd = Command::Set {
-            key: "foo".to_string(),
-            val: "bar".into(),
-            resp: resp_tx,
-        };
-        tx2.send(cmd).await.unwrap();
-        let resp = resp_rx.await;
-        print!("got {:?}\n", resp);
-    });
-
-    let manager = tokio::spawn(async move {
-
-        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-
-        while let Some(cmd) = rx.recv().await {
-            use Command::*;
-
-            match cmd {
-                Get { key, resp } => {
-                    let res = client.get(&key).await;
-                    let _ = resp.send(res);
-                },
-                Set { key, val, resp } => {
-                    let v = val.clone();
-                    let res = client.set(&key, val).await;
-                    let _ = resp.send(res);
-                }
-            }
-             
-        }
-    });
-    
-    t1.await.unwrap();
-    t2.await.unwrap();
-    manager.await.unwrap();
+    let out = future.await;
+    assert_eq!(out, "done");
 }
